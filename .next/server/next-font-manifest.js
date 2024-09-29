98ed435f0c7ec90813821028a5496f29aa6b3365
SofianBdr <sofianboudraa.msr@gmail.com>
2024-09-29 10:36:11 +0200

    cp7

--- .next/server/next-font-manifest.js
+++ .next/server/next-font-manifest.js
@@ -1,1 +1,1 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/home/sofian/Documents/NextJs-learning-course/app/layout\":[\"static/media/a34f9d1faa5f3315-s.p.woff2\",\"static/media/c556ae4be4c9cfa8-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
+self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{},\"appUsingSizeAdjust\":false,\"pagesUsingSizeAdjust\":false}"
